3c439005e048df7ff2eb57f5babf160246074642
GalimovKi <galimov.ki@phystech.edu>
2023-11-29 23:15:30 +0300

    Загружаю проект в ветку dev

---
+++ Database.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+async def conn_start():
+    global conn, cur
+    conn = sqlite3.connect('unreal_kirill.db')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS users("
+            "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+            " phone_number TEXT, "
+            "email TEXT, "
+            "in_payload TEXT)")
+    conn.commit()
+
+
+async def add_user(phone_number, email, in_payload):
+    cur.execute("INSERT INTO users (phone_number, email, in_payload) "
+                "VALUES ('%s','%s','%s')" % (phone_number, email, in_payload))
+    conn.commit()
